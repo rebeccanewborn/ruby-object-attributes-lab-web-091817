8d357765f12fd1bf96902a340d6f40f4c1c56856
rebeccanewborn <rjn58@cornell.edu>
2017-09-20 10:33:33 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(arg)
+    @name = arg
+  end
+  def name
+    @name
+  end
+
+  def breed=(arg)
+    @breed = arg
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(arg)
+    @name = arg
+  end
+  def name
+    @name
+  end
+
+  def job=(arg)
+    @job = arg
+  end
+  def job
+    @job
+  end
+end
